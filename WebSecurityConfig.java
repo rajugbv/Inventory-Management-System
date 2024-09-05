8891ab7a77b189d374ccc00ccddae0141eec9f6a
rajugbv <41448193+rajugbv@users.noreply.github.com>
2024-09-04 22:58:47 -0400

    Create WebSecurityConfig.java

---
+++ WebSecurityConfig.java
@@ -0,0 +1,13 @@
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/api/**").authenticated()
+            .and()
+            .httpBasic();
+    }
+}
